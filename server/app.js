30a88461ead413c1593c76d7b49aced63ffb2bc7
Jigga Byte <jigga.e10@gmail.com>
2018-10-29 06:52:38 +0100

    Update commit

--- server/app.js
+++ server/app.js
@@ -1,6 +1,15 @@
 const express = require("express");
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
 
 const PORT = process.env.PORT || 5000;
-const app = express();
+
+const router = require('./routes/route.js');
+
+app.use('/', router);
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
